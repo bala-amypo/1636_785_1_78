3fc1708a4b694096a99437c889df7f09b3fa6cf1
bala-amypo <bala@amypo.in>
2025-12-20 16:40:16 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/TaskRecordService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import java.util.List;
+import com.example.demo.entity.TaskRecord;
+
+public interface TaskRecordService {
+
+    TaskRecord createTask(TaskRecord task);
+
+    TaskRecord updateTask(Long id, TaskRecord updated);
+
+    List<TaskRecord> getOpenTasks();
+
+    TaskRecord getTaskByCode(String code);
+
+    List<TaskRecord> getAllTasks();
+}
---
+++ src/main/java/com/example/demo/service/impl/TaskRecordServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+import com.example.demo.entity.TaskRecord;
+import com.example.demo.repository.TaskRecordRepository;
+import com.example.demo.service.TaskRecordService;
+
+@Service
+public class TaskRecordServiceImpl implements TaskRecordService {
+
+    @Autowired
+    TaskRecordRepository repo;
+
+    public TaskRecord createTask(TaskRecord task) {
+        return repo.save(task);
+    }
+
+    public TaskRecord updateTask(Long id, TaskRecord updated) {
+        TaskRecord t = repo.findById(id).orElse(null);
+        if (t != null) {
+            t.setTaskName(updated.getTaskName());
+            t.setRequiredSkill(updated.getRequiredSkill());
+            t.setRequiredSkillLevel(updated.getRequiredSkillLevel());
+            t.setPriority(updated.getPriority());
+            t.setStatus(updated.getStatus());
+            return repo.save(t);
+        }
+        return null;
+    }
+
+    public List<TaskRecord> getOpenTasks() {
+        return repo.findByStatus("OPEN");
+    }
+
+    public TaskRecord getTaskByCode(String code) {
+        return repo.findByTaskCode(code);
+    }
+
+    public List<TaskRecord> getAllTasks() {
+        return repo.findAll();
+    }
+}
