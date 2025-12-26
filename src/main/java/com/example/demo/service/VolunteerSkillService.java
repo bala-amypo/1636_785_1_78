d839d6b4cdabe8a39b54da1ff20a1b96540025d4
bala-amypo <bala@amypo.in>
2025-12-26 05:12:47 +0000

    User commit

--- src/main/java/com/example/demo/service/VolunteerSkillService.java
+++ src/main/java/com/example/demo/service/VolunteerSkillService.java
@@ -3,6 +3,9 @@ package com.example.demo.service;
 import com.example.demo.model.VolunteerSkillRecord;
 import java.util.List;
 
-public interface VolunteerSkillRecordService {
-    List<VolunteerSkillRecord> getSkillsByVolunteerId(String volunteerId);
+public interface VolunteerSkillService {
+
+    VolunteerSkillRecord addOrUpdateSkill(VolunteerSkillRecord skill);
+
+    List<VolunteerSkillRecord> getSkillsByVolunteer(Long volunteerId);
 }
